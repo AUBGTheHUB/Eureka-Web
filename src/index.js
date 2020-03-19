7b1c4e9ae8445772e2c078a564ce02bb72f7858c
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2020-03-20 01:25:44 +0200

    Added navbar

--- src/index.js
+++ src/index.js
@@ -4,43 +4,43 @@ import ReactDOM from 'react-dom';
 import { NavDropdown } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
-
+import NavbarUnimorph from './navbar'
 const App = () =>(
     <div>
         Hello world!!!!!!!!!!!!!!!!!!!!!!!!!!
     </div>
 )
 
-function App1() {
-  return (
-        <div className="App">
-            <Navbar bg="light" variant="dark">
-                <Navbar.Brand href="#home">
-                    <img
-                        src="/src/Mu_logo3.svg"
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                        alt="React Bootstrap logo"
-                    />
-                </Navbar.Brand>
+// function NavbarUnimorph() {
+//   return (
+//         <div className="NavbarUnimorph">
+//             <Navbar bg="dark" variant="dark">
+//                 <Navbar.Brand href="#home">
+//                     <img
+//                         src="/src/Mu_logo3.svg"
+//                         width="30"
+//                         height="30"
+//                         className="d-inline-block align-top"
+//                         alt="React Bootstrap logo"
+//                     />
+//                 </Navbar.Brand>
 
-                <Nav className="mr-auto">
-                </Nav>
+//                 <Nav className="mr-auto">
+//                 </Nav>
 
-                <Nav>
-                    <Nav.Link href="#about">About the project</Nav.Link>
-                    <Nav.Link href="#about">Download</Nav.Link>
-                    <Nav.Link href="#contact">Contact Us</Nav.Link>
-                    <Nav.Link href="#account">My Account</Nav.Link>
-                </Nav>
+//                 <Nav>
+//                     <Nav.Link href="#about">About the project</Nav.Link>
+//                     <Nav.Link href="#about">Download</Nav.Link>
+//                     <Nav.Link href="#contact">Contact Us</Nav.Link>
+//                     <Nav.Link href="#account">My Account</Nav.Link>
+//                 </Nav>
 
-            </Navbar>
-        </div>
- );
-}
+//             </Navbar>
+//         </div>
+//  );
+// }
 
 ReactDOM.render(
-    <App1/>,
+    <NavbarUnimorph/>,
     document.getElementById('root')
 )
--- src/navbar.js
+++ src/navbar.js
@@ -4,15 +4,27 @@ import { Navbar } from 'react-bootstrap';
 function NavbarUnimorph() {
     return (
           <div className="NavbarUnimorph">
-              <Navbar>
-  <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-  <Navbar.Toggle />
-  <Navbar.Collapse className="justify-content-end">
-    <Navbar.Text>
-      Signed in as: <a href="#login">Mark Otto</a>
-    </Navbar.Text>
-  </Navbar.Collapse>
-</Navbar>
+              <Navbar bg="dark" variant="dark">
+                  <Navbar.Brand href="#home">
+                      <img
+                          src="/src/Mu_logo3.svg"
+                          width="30"
+                          height="30"
+                          className="d-inline-block align-top"
+                          alt="React Bootstrap logo"
+                      />
+                  </Navbar.Brand>
+  
+                  <Nav className="mr-auto">
+                  </Nav>
+  
+                  <Nav>
+                      <Nav.Link href="#about">About the project</Nav.Link>
+                      <Nav.Link href="#about">Download</Nav.Link>
+                      <Nav.Link href="#contact">Contact Us</Nav.Link>
+                      <Nav.Link href="#account">My Account</Nav.Link>
+                  </Nav>
+  
+              </Navbar>
           </div>
    );
-  }
