e41f35c8cd72c8eb4240ece4562926e41655ec46
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2020-03-20 01:22:34 +0200

    Added navbar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { NavDropdown } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+
 const App = () =>(
     <div>
         Hello world!!!!!!!!!!!!!!!!!!!!!!!!!!
     </div>
 )
 
+function App1() {
+  return (
+        <div className="App">
+            <Navbar bg="light" variant="dark">
+                <Navbar.Brand href="#home">
+                    <img
+                        src="/src/Mu_logo3.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    />
+                </Navbar.Brand>
+
+                <Nav className="mr-auto">
+                </Nav>
+
+                <Nav>
+                    <Nav.Link href="#about">About the project</Nav.Link>
+                    <Nav.Link href="#about">Download</Nav.Link>
+                    <Nav.Link href="#contact">Contact Us</Nav.Link>
+                    <Nav.Link href="#account">My Account</Nav.Link>
+                </Nav>
+
+            </Navbar>
+        </div>
+ );
+}
 
 ReactDOM.render(
-    <App/>,
+    <App1/>,
     document.getElementById('root')
 )
---
+++ src/navbar.js
@@ -0,0 +1,18 @@
+import { Navbar } from 'react-bootstrap';
+
+
+function NavbarUnimorph() {
+    return (
+          <div className="NavbarUnimorph">
+              <Navbar>
+  <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+  <Navbar.Toggle />
+  <Navbar.Collapse className="justify-content-end">
+    <Navbar.Text>
+      Signed in as: <a href="#login">Mark Otto</a>
+    </Navbar.Text>
+  </Navbar.Collapse>
+</Navbar>
+          </div>
+   );
+  }
