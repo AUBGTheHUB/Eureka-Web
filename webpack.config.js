af9451ed0c8d2c47b84d1547733ad18754ee3663
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2020-03-21 23:15:05 +0200

    Implemented Logo Image Component

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,19 @@ module.exports = {
             exclude: /node_modules/,
             test: /\.js$/,
             loader: 'babel-loader'
-        }]
+        },
+        {
+            exclude: /node_modules/,
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+              ]
+        }
+        ],
     },
     plugins:[
         new hwp({template:path.join(__dirname, '/src/index.html')})
     ]
 }
+
