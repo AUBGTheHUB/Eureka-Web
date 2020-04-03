3dc9f602ce0e1e992ba2c3dca60f29b04245d700
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2020-04-04 02:47:18 +0300

    Centered Button

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path    = require('path');
 var hwp     = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.join(__dirname, '/src/index.js'),
+    entry: path.join(__dirname, '/src/languages.js'),
     output: {
         filename: 'build.js',
         path: path.join(__dirname, '/dist')
@@ -24,7 +24,7 @@ module.exports = {
         ],
     },
     plugins:[
-        new hwp({template:path.join(__dirname, '/src/index.html')})
+        new hwp({template:path.join(__dirname, '/src/languages.html')})
     ]
 }
 
