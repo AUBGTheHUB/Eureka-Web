2ce6a6d62bfef05ab2c4cd41d3e9d22705dd1ef3
Dastan <dastanbeksamatov@gmail.com>
2020-09-01 00:59:18 +0300

    Two columns for lemma

--- src/components/lemma/LemmaTableV1.js
+++ src/components/lemma/LemmaTableV1.js
@@ -8,47 +8,60 @@ import lemmaService from '../../services/lemma';
 const LemmaTableV1 = (props) => {
     const [data, setData] = useState([]);
     const [columns, setColumns] = useState([]);
+    const [pos, setPos] = useState([]);
     const [showDialog, setShowDialog] = useState(false);
 
     // process the data from api and format it for the table
     useEffect(() => {
-    const getData = async () => {
-        const lemma = await lemmaService.getLemma(props.lemma);
-        setData(lemma);
-        setColumns(lemma.related_words[0].dimensions.Tense);
-    }
-    getData();
+        const getData = async () => {
+            const lemma = await lemmaService.getLemma(props.lemma);
+            setData(lemma.related_words);
+            setPos(lemma.pos);
+            const cols = new Set([]);
+            lemma.related_words.forEach(word => {
+                word.tagset.features.forEach(feat => {
+                    if(feat.dimension.name === 'Tense'){
+                        cols.add(feat.name);
+                    }
+                })
+            })
+            setColumns(Array.from(cols));
+        }
+        getData();
     }, [])
-    console.log(data);
-    console.log(columns);
+
+    if(!columns){
+        return null;
+    }
     return (
-        <div class="container">
-            <div class="row">
-                <div class="col-md">
+        <div className="container">
+            <div className="row">
+                <div className="col-md">
                     <Table striped bordered hover>
                         <thead>
                             <tr>
-                                {columns.slice(0, columns.length/2).map(tense => {
+                                {columns.slice(0, columns.length/2).map((tense, i) => {
                                     return(
-                                        <th>{tense}</th>
+                                        <th key={i}>{tense}</th>
                                     )
                                 })}
                             </tr>
                         </thead>
                         <tbody>
-                            {/* {data.map(word => {
-                                <>
+                            {/* {data.map((word, i) => {
+                                if(word.tagset.features)
                             })} */}
+                            <pre>{JSON.stringify(data, null, 2)}</pre>
                         </tbody>
                     </Table>
                 </div>
-                <div class="col-md">
+                <div className="col-md">
                     <Table striped bordered hover>
                         <thead>
                             <tr>
-                                {columns.slice(columns.length/2).map(tense => {
+                                {columns.slice(columns.length/2).map((tense, i) => {
                                     return(
-                                        <th>{tense}</th>
+                                        <th key={i}>{tense}</th>
                                     )
                                 })}
                             </tr>
