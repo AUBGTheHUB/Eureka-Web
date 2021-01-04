788aa83338f8508027f6deb0f5949dffc1d5c214
Dastan <dastanbeksamatov@gmail.com>
2021-01-04 23:13:42 +0200

    Some changes

--- src/store/reducers/languageReducer.js
+++ src/store/reducers/languageReducer.js
@@ -8,7 +8,7 @@ const initialState = [
 const languageReducer = (state = initialState, action) => {
     switch (action.type){
         case 'GET_ALL_LANGUAGES':
-            return state.concat(action.data.results);
+            return state.concat(action.data);
         default:
             return state;
     }
